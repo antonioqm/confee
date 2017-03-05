7aed7949ebb4b9b37d1b4c24059f3dc78aff8bb2
AntonioQM <antonioqm3@gmail.com>
2017-03-05 11:07:23 -0500

    iniciando a aplicação

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading" v-text=name></div>
+                    <div class="panel-body">
+                        <input class="form-control" type="text" name="name" v-model=name>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+
+            return {
+                name: 'EU amo Vue 2'
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
